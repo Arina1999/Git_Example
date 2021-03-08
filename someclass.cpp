f77dc03766354815389ff2b84601480eb06cd5e2
Arina1999 <stok1712@gmail.com>
2021-03-08 20:29:03 +0700

    Реализован класс SomeClass

---
+++ someclass.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "someclass.h"
+
+SomeClass::SomeClass(const bool someBoolData,const int someIntData)
+{
+  m_someBoolData = someBoolData;
+  m_someIntData = someIntData;
+}
+
+bool SomeClass::someBoolData() const
+{
+   return m_someBoolData;
+}
+
+int SomeClass::someIntData() const
+{
+   return m_someIntData;
+}
+
---
+++ someclass.h
@@ -0,0 +1,20 @@
+#ifndef SOMECLASS_H
+#define SOMECLASS_H
+
+
+class SomeClass
+{
+public:
+    SomeClass(const bool someBoolData , const int someIntData);
+
+    bool someBoolData() const;
+    int someIntData() const;
+
+
+
+private:
+    bool m_someBoolData;
+     int m_someIntData;
+};
+
+#endif // SOMECLASS_H
